ea23192b39d25eacd570a53155d1227d01e3690d
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-04-30 11:06:50 +0200

    extracted to component

--- src/components/List.js
+++ src/components/List.js
@@ -1,35 +1,16 @@
 import 'animate.css'
+import ListItem from './ListItem'
 
-function List({ memberList, handleEdit, deleteMember }) {
+function List({ memberList, handleEdit, deleteMember, selectedId }) {
   return (
     <div>
       <ul className="list">
         {memberList?.length > 0 ? (
           memberList.map((member) => (
-            <li
-              id={member.id}
-              className="card animate__animated animate__fadeInLeft animate__delay-1s"
+            <ListItem
               key={member.id}
-            >
-              <p className="card_title">Name: {member.name}</p>
-              <p>Age: {member.age}</p>
-              <div className="btn-group">
-                <button
-                  className="btn"
-                  onClick={handleEdit}
-                  data-id={member.id}
-                >
-                  update
-                </button>
-                <button
-                  className="btn secondary"
-                  onClick={deleteMember}
-                  data-id={member.id}
-                >
-                  delete
-                </button>
-              </div>
-            </li>
+              {...{ member, handleEdit, deleteMember, selectedId }}
+            />
           ))
         ) : (
           <p>No members yet. Let's create one...</p>
---
+++ src/components/ListItem.js
@@ -0,0 +1,30 @@
+function ListItem({ member, handleEdit, deleteMember, selectedId }) {
+  return (
+    <li
+      id={member.id}
+      className={
+        selectedId === member.id
+          ? ' editBorder card animate__animated animate__fadeInLeft animate__delay-1s'
+          : ' card animate__animated animate__fadeInLeft animate__delay-1s'
+      }
+      key={member.id}
+    >
+      <p className="card_title">Name: {member.name}</p>
+      <p>Age: {member.age}</p>
+      <div className="btn-group">
+        <button className="btn" onClick={handleEdit} data-id={member.id}>
+          update
+        </button>
+        <button
+          className="btn secondary"
+          onClick={deleteMember}
+          data-id={member.id}
+        >
+          delete
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default ListItem
